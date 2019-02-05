b5ec522eff71993ee061da8c4be525e8c7876c3f
Ryan Herbst <rherbst@slac.stanford.edu>
2019-02-05 00:02:23 -0800

    Map Fix

--- include/DmaDriver.h
+++ include/DmaDriver.h
@@ -92,7 +92,6 @@ struct DmaRegisterData {
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
-#include <errno.h>
 #include <sys/ioctl.h>
 #include <sys/signal.h>
 #include <sys/fcntl.h>
@@ -292,8 +291,9 @@ static inline void ** dmaMapDma(int32_t fd, uint32_t *count, uint32_t *size) {
    void *   temp;
    void **  ret;
    uint32_t bCount;
+   uint32_t gCount;
    uint32_t bSize;
-   uint32_t x;
+   off_t    offset;
 
    bSize  = ioctl(fd,DMA_Get_Buff_Size,0);
    bCount = ioctl(fd,DMA_Get_Buff_Count,0);
@@ -301,22 +301,22 @@ static inline void ** dmaMapDma(int32_t fd, uint32_t *count, uint32_t *size) {
    if ( count != NULL ) *count = bCount;
    if ( size  != NULL ) *size  = bSize;
 
-   if ( (ret = (void **)malloc(sizeof(void *) * bCount)) == 0 ) {
-      //printf("Failed to map holding buffer\n");
-      return(NULL);
+   if ( (ret = (void **)malloc(sizeof(void *) * bCount)) == 0 ) return(NULL);
+
+   // Attempt to map
+   while ( gCount < bCount ) {
+      offset = (off_t)bSize * (off_t)gCount;
+
+      if ( (temp = mmap (0, bSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset)) == MAP_FAILED) break;
+      ret[gCount++] = temp;
    }
 
-   for (x=0; x < bCount; x++) {
-
-      if ( (temp = mmap (0, bSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (bSize*x))) == MAP_FAILED) {
-         free(ret);
-         //printf("Failed to map buffer %i. errno=%i\n",x,errno);
-         return(NULL);
-      }
-
-      ret[x] = temp;
+   // Map failed
+   if ( gCount != bCount ) {
+      while ( gCount != 0 ) munmap(ret[--gCount],bSize);
+      free(ret);
+      ret = NULL;
    }
-
    return(ret);
 }
 
