1b0e4ca0dc098e2118262e98cf5c4db7fb0b6f27
Larry Ruckman <ruckman@slac.stanford.edu>
2024-02-23 12:55:12 -0800

    applying 'Kernel-doc' style to include header files

--- include/DataDriver.h
+++ include/DataDriver.h
@@ -1,28 +1,27 @@
 /**
  *-----------------------------------------------------------------------------
- * Title      : Data Development Card Driver, Shared Header
- * ----------------------------------------------------------------------------
- * File       : DataDriver.h
- * Created    : 2017-03-21
- * ----------------------------------------------------------------------------
+ * Company: SLAC National Accelerator Laboratory
+ *-----------------------------------------------------------------------------
  * Description:
- * Defintions and inline functions for interacting with Data Development driver.
- * ----------------------------------------------------------------------------
- * This file is part of the aes_stream_drivers package. It is subject to
- * the license terms in the LICENSE.txt file found in the top-level directory
- * of this distribution and at:
- *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
- * No part of the aes_stream_drivers package, including this file, may be
- * copied, modified, propagated, or distributed except according to the terms
- * contained in the LICENSE.txt file.
- * ----------------------------------------------------------------------------
-**/
+ *    Definitions and inline functions for interacting with the Data Development driver
+ *-----------------------------------------------------------------------------
+ * License:
+ *    This file is subject to the license terms in the LICENSE.txt file found
+ *    in the top-level directory of this distribution and at:
+ *       https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ *
+ *    No part of the aes_stream_drivers package, including this file, may be
+ *    copied, modified, propagated, or distributed except according to the terms
+ *    contained in the LICENSE.txt file.
+ *-----------------------------------------------------------------------------
+ */
+
 #ifndef __DATA_DRIVER_H__
 #define __DATA_DRIVER_H__
-#include <AxisDriver.h>
-#include <DmaDriver.h>
-#include <FpgaProm.h>
-#include <AxiVersion.h>
 
-#endif
+// Include dependencies for Data Development driver functionality
+#include <AxisDriver.h>  // Axis Communication Driver
+#include <DmaDriver.h>   // Direct Memory Access (DMA) Driver
+#include <AxiVersion.h>  // Kernel AXI Version Interface
 
+#endif // __DATA_DRIVER_H__
