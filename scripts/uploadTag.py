6a98f6f4c4eba5cff3c02318a92b3dc3d20f2dc2
Larry Ruckman <ruckman@slac.stanford.edu>
2024-02-23 20:23:04 -0800

    adding license header

--- scripts/uploadTag.py
+++ scripts/uploadTag.py
@@ -1,3 +1,18 @@
+# ----------------------------------------------------------------------------
+# Company    : SLAC National Accelerator Laboratory
+# ----------------------------------------------------------------------------
+# Description:
+#     Python code that called during Github Actions to upload tag
+# ----------------------------------------------------------------------------
+# This file is part of the aes_stream_drivers package. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the aes_stream_drivers package, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+# ----------------------------------------------------------------------------
+
 import sys
 import os
 import argparse
