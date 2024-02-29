faf8f9a10b40450c5f88accea430652e7b01701e
Larry Ruckman <ruckman@slac.stanford.edu>
2024-02-28 21:17:22 -0800

    use readl() instead ioread32()
    These changes replace the direct I/O access functions with the recommended memory-mapped I/O functions, improving portability and security within the kernel module.

--- data_gpu/driver/src/data_gpu_top.c
+++ data_gpu/driver/src/data_gpu_top.c
@@ -211,9 +211,9 @@ int DataGpu_Probe(struct pci_dev *pcidev, const struct pci_device_id *dev_id) {
    AxiVersion_SetUserReset(dev->base + AVER_OFF,false);
 
    // 128bit desc, = 64-bit address map
-   if ( (ioread32(dev->reg) & 0x10000) != 0) {
+   if ( (readl(dev->reg) & 0x10000) != 0) {
       // Get the AXI Address width (in units of bits)
-      axiWidth = (ioread32(dev->reg+0x34) >> 8) & 0xFF;
+      axiWidth = (readl(dev->reg+0x34) >> 8) & 0xFF;
       if ( !dma_set_mask_and_coherent(dev->device, DMA_BIT_MASK(axiWidth)) ) {
          dev_info(dev->device,"Init: Using %d-bit DMA mask.\n",axiWidth);
       } else {
@@ -226,7 +226,7 @@ int DataGpu_Probe(struct pci_dev *pcidev, const struct pci_device_id *dev_id) {
 
    dev_info(dev->device,"Init: Reg  space mapped to 0x%llx.\n",(uint64_t)dev->reg);
    dev_info(dev->device,"Init: User space mapped to 0x%llx with size 0x%x.\n",(uint64_t)dev->rwBase,dev->rwSize);
-   dev_info(dev->device,"Init: Top Register = 0x%x\n",ioread32(dev->reg));
+   dev_info(dev->device,"Init: Top Register = 0x%x\n",readl(dev->reg));
 
    return(0);
 }
