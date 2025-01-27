020edb1034daa61f961d75487e6a1a8bded3068f
mmishra9 <mmishra9@slac.stanford.edu>
2025-01-27 14:02:30 -0800

    fixed linter error

--- common/driver/gpu_async.c
+++ common/driver/gpu_async.c
@@ -171,8 +171,8 @@ int32_t Gpu_AddNvidia(struct DmaDevice *dev, uint64_t arg) {
          // Update buffer count and write DMA addresses to device
          if (buffer->write) {
             writel(buffer->dmaMapping->dma_addresses[0] & 0xFFFFFFFF, data->base+0x100+data->writeBuffers.count*16);
-            writel((buffer->dmaMapping->dma_addresses[0] >> 32) & 0xFFFFFFFF, data->base+0x104+data->writeBuffers.count*16); 
-            writel(mapSize - BUFFER_OFFSET, data->base+0x108+data->writeBuffers.count*16); // subtracted BUFFER_OFFSET to align the mapSize with maxSize in firmware
+            writel((buffer->dmaMapping->dma_addresses[0] >> 32) & 0xFFFFFFFF, data->base+0x104+data->writeBuffers.count*16);
+            writel(mapSize - BUFFER_OFFSET, data->base+0x108+data->writeBuffers.count*16);  // Subtracted BUFFER_OFFSET to align the mapSize with maxSize in firmware
             data->writeBuffers.count++;
          } else {
             writel(buffer->dmaMapping->dma_addresses[0] & 0xFFFFFFFF, data->base+0x200+data->readBuffers.count*16);
