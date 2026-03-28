953a37d5cbfe9f217eacc19b61d7b059be4b961a
Larry Ruckman <ruckman@slac.stanford.edu>
2026-03-27 20:14:25 -0700

    docs: fix sphinx build warnings in conf.py
    - Remove display_version from html_theme_options (unsupported in sphinx_rtd_theme 3.x)
    - Remove html_static_path entry for non-existent _static dir (create empty dir instead)

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,6 @@ html_theme_options = {
     'sticky_navigation': True,
     'includehidden': True,
     'logo_only': False,
-    'display_version': True,
     'prev_next_buttons_location': 'bottom',
 }
 
